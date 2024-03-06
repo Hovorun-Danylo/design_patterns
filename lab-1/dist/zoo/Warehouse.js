5cad36dedc4d3a70d5f2790fe7cd8f731ae499f2
Hovorun-Danylo <vt222_gdd@student.ztu.edu.ua>
2024-03-06 08:45:07 +0200

    warehouse implementation

--- lab-1/dist/zoo/Warehouse.js
+++ lab-1/dist/zoo/Warehouse.js
@@ -1,23 +1,43 @@
+import { Food } from "../food/Food.js";
 export class Warehouse {
     constructor(factory, foodTypes) {
         this.stock = new Map();
         this.evaluateStock(factory, foodTypes);
     }
+    addFood(consumable) {
+        if (this.stock.has(consumable.foodType)) {
+            const existingConsumable = this.stock.get(consumable.foodType);
+            existingConsumable.weightInGrams += consumable.weightInGrams;
+            return;
+        }
+        this.stock.set(consumable.foodType, consumable);
+    }
+    pullOut(consumableType, weightInGrams) {
+        if (weightInGrams <= 0) {
+            throw Error("Pulling weight can't be <= 0!");
+        }
+        const availableWeight = this.getAvailableWeight(consumableType);
+        if (availableWeight < weightInGrams) {
+            throw Error(`Not enough ${consumableType} in stock!`);
+        }
+        this.stock.get(consumableType).weightInGrams -= weightInGrams;
+        return new Food(consumableType, weightInGrams);
+    }
+    getAvailableWeight(consumableType) {
+        if (this.stock.has(consumableType))
+            return this.stock.get(consumableType).weightInGrams;
+        return 0;
+    }
+    isAvailable(consumableType) {
+        return Boolean(this.getAvailableWeight(consumableType));
+    }
     evaluateStock(factory, foodTypes) {
         for (let catalogue of foodTypes) {
             for (let foodType of Object.values(catalogue.items)) {
                 let food = factory.create(foodType);
-                this.addFood(this.stock, food);
+                this.addFood(food);
             }
         }
-    }
-    addFood(stock, consumable) {
-        if (stock.has(consumable.foodType)) {
-            const existingConsumable = stock.get(consumable.foodType);
-            existingConsumable.weightInGrams += consumable.weightInGrams;
-            return;
-        }
-        stock.set(consumable.foodType, consumable);
     }
 }
 //# sourceMappingURL=Warehouse.js.map
--- lab-1/src/zoo/Warehouse.ts
+++ lab-1/src/zoo/Warehouse.ts
@@ -1,33 +1,65 @@
 
 import { ICatalogue } from "../abstract/Catalogue.js";
-import { IFood } from "../food/Food.js";
+import { Food, IFood } from "../food/Food.js";
 import { IFoodFactory } from "../food/factory/IFactory.js";
 import { IFoodType } from "../food/FoodType.js";
 
-export class Warehouse {
+export interface IWarehouse {
+    addFood(consumable: IFood<any>): void
+    pullOut<T extends string>(consumableType: IFoodType<T>, weightInGrams: number): IFood<T>
+    getAvailableWeight(consumableType: IFoodType<any>): number
+    isAvailable(consumableType: IFoodType<any>): boolean
+}
+
+export class Warehouse implements IWarehouse {
     private stock: Map<IFoodType<any>, IFood<any>> = new Map()
 
     constructor(factory: IFoodFactory, foodTypes: Iterable<ICatalogue<IFoodType<any>>>) {
         this.evaluateStock(factory, foodTypes)
     }
 
-    private evaluateStock(factory: IFoodFactory, foodTypes: Iterable<ICatalogue<IFoodType<any>>>) {
-        for (let catalogue of foodTypes) {
-            for (let foodType of Object.values(catalogue.items)) {
-                let food = factory.create(foodType)
-                this.addFood(this.stock, food)
-            }
-        }
-    }
-
-    private addFood(stock: Map<IFoodType<any>, IFood<any>>, consumable: IFood<any>) {
-        if (stock.has(consumable.foodType)) {
-            const existingConsumable = stock.get(consumable.foodType)!
+    addFood(consumable: IFood<any>) {
+        if (this.stock.has(consumable.foodType)) {
+            const existingConsumable = this.stock.get(consumable.foodType)!
             existingConsumable.weightInGrams += consumable.weightInGrams
 
             return
         }
 
-        stock.set(consumable.foodType, consumable)
+        this.stock.set(consumable.foodType, consumable)
+    }
+
+    pullOut<T extends string>(consumableType: IFoodType<T>, weightInGrams: number): IFood<T> {
+        if (weightInGrams <= 0) {
+            throw Error("Pulling weight can't be <= 0!")
+        }
+
+        const availableWeight = this.getAvailableWeight(consumableType)
+        if (availableWeight < weightInGrams) {
+            throw Error(`Not enough ${consumableType} in stock!`)
+        }
+
+        this.stock.get(consumableType)!.weightInGrams -= weightInGrams
+        return new Food(consumableType, weightInGrams)
+    }
+
+    getAvailableWeight(consumableType: IFoodType<any>): number {
+        if (this.stock.has(consumableType))
+            return this.stock.get(consumableType)!.weightInGrams
+
+        return 0;
+    }
+
+    isAvailable(consumableType: IFoodType<any>): boolean {
+        return Boolean(this.getAvailableWeight(consumableType))
+    }
+
+    private evaluateStock(factory: IFoodFactory, foodTypes: Iterable<ICatalogue<IFoodType<any>>>) {
+        for (let catalogue of foodTypes) {
+            for (let foodType of Object.values(catalogue.items)) {
+                let food = factory.create(foodType)
+                this.addFood(food)
+            }
+        }
     }
 }
