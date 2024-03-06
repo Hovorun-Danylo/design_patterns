2ba68e236e6a919c8a37768384a522a75a013684
Hovorun-Danylo <vt222_gdd@student.ztu.edu.ua>
2024-03-06 13:42:39 +0200

    minor cleanup

--- lab-1/src/main.ts
+++ lab-1/src/main.ts
@@ -5,14 +5,7 @@ import { RandomFoodFactory } from "./food/factory/RandomFoodFactory.js";
 import { OmnivoreFood } from "./config.js";
 
 const randomFoodFactory = new RandomFoodFactory(500, 2000)
-
 const warehouse = new Warehouse(randomFoodFactory, OmnivoreFood)
-
-const fish = randomFoodFactory.create(OmnivoreFood.fish)
-const hay = randomFoodFactory.create(OmnivoreFood.hay)
-
-warehouse.addFood(fish)
-warehouse.addFood(hay)
 
 const zoo = new Zoo(warehouse)
 zoo.app()
