091a11a7c690a821347312104f42b48101c69ff7
Hovorun-Danylo <vt222_gdd@student.ztu.edu.ua>
2024-03-07 07:35:42 +0200

    Employee implementation

--- lab-1/dist/employee/Employee.js
+++ lab-1/dist/employee/Employee.js
@@ -1,3 +1,10 @@
 export class Employee {
+    constructor(name, position) {
+        this.name = name;
+        this.position = position;
+    }
+    feedAnimal(animal, food) {
+        animal.feedAssortment(food);
+    }
 }
 //# sourceMappingURL=Employee.js.map
--- lab-1/src/employee/Employee.ts
+++ lab-1/src/employee/Employee.ts
@@ -1,4 +1,18 @@
 
-export class Employee {
-    
+import { IFood } from "../food/Food.js";
+import { IFeedable } from "../animals/Animal.js";
+
+export interface IEmployee<T extends string> {
+    readonly name: string
+    readonly position: T
+
+    feedAnimal(animal: IFeedable, food: Iterable<IFood<any>>): void
+}
+
+export class Employee<T extends string> implements IEmployee<T> {
+    constructor(readonly name: string, readonly position: T) { }
+
+    feedAnimal(animal: IFeedable, food: Iterable<IFood<any>>) {
+        animal.feedAssortment(food)
+    }
 }
