2f267f1af9a0a163a38f7f2065006232d00471a5
Hovorun-Danylo <vt222_gdd@student.ztu.edu.ua>
2024-03-07 08:42:34 +0200

    initial zoo commit

--- lab-1/dist/zoo/Zoo.js
+++ lab-1/dist/zoo/Zoo.js
@@ -1,19 +1,37 @@
+import { Animal } from "../animals/Animal.js";
+import { HerbivoreSpecies, OmnivoreFood } from "../config.js";
+import { Food } from "../food/Food.js";
+import { Enclosure } from "../enclosure/Enclosure.js";
+import { Employee } from "../employee/Employee.js";
 export class Zoo {
     constructor(warehouse) {
         this.warehouse = warehouse;
+        this.animals = new Set();
+        this.enclosures = new Set();
+        this.employees = new Set();
+        this.registerMap = new Map([
+            ["animal", this.animals],
+            ["enclosure", this.enclosures],
+            ["employee", this.employees],
+        ]);
+    }
+    register(entityType, entity) {
+        this.registerMap.get(entityType)?.add(entity);
     }
     app() {
-        // const bobik = new Animal("bobik", HerbivoreSpecies.elephant)
-        // const bobik2 = new Animal("bobik2", HerbivoreSpecies.elephant)
-        //
-        // const hay = new Food(HerbivoreDiet.hay, 50)
-        // const vegetables = new Food(HerbivoreDiet.vegetables, 100)
-        //
-        // const enclosure = new Enclosure(2, [ bobik, bobik2 ])
-        //
-        // const assortment = [ hay, vegetables ]
-        // bobik.feedAssortment(assortment)
-        // enclosure.showInhabitants()
+        const bob = new Employee("Bob", "Animal Curator");
+        this.register("employee", bob);
+        const bobik = new Animal("bobik", HerbivoreSpecies.elephant);
+        const bobik2 = new Animal("bobik2", HerbivoreSpecies.elephant);
+        const hay = new Food(OmnivoreFood.hay, 50);
+        const vegetables = new Food(OmnivoreFood.vegetables, 100);
+        const enclosure = new Enclosure(2, [bobik, bobik2]);
+        const assortment = [hay, vegetables];
+        bob.feedAnimal(bobik, assortment);
+        enclosure.showInhabitants();
+    }
+    restock(factory, foodTypes) {
+        this.warehouse.restock(factory, foodTypes);
     }
 }
 //# sourceMappingURL=Zoo.js.map
--- lab-1/src/zoo/Zoo.ts
+++ lab-1/src/zoo/Zoo.ts
@@ -1,23 +1,56 @@
 
-import {IRestockable, IWarehouse} from "./Warehouse.js";
+import { IRestockable, IWarehouse } from "./Warehouse.js";
+import { Animal, IAnimal } from "../animals/Animal.js";
+import { HerbivoreSpecies, OmnivoreFood } from "../config.js";
+import { Food } from "../food/Food.js";
+import { Enclosure } from "../enclosure/Enclosure.js";
+import { Employee, IEmployee } from "../employee/Employee.js";
+import { IFoodFactory } from "../food/factory/IFactory.js";
+import { ICatalogue } from "../abstract/Catalogue.js";
+import { getFoodTypes } from "../abstract/types.js";
+
+type EntityTypeMap = {
+    animal: IAnimal<any>;
+    enclosure: Set<IAnimal<any>>;
+    employee: IEmployee<any>;
+};
 
 export class Zoo<T extends string> {
-    constructor(private warehouse: IWarehouse<T> & IRestockable<T>) {
-        
+    private animals: Set<IAnimal<any>> = new Set()
+    private enclosures: Set<Set<IAnimal<any>>> = new Set()
+    private employees: Set<IEmployee<any>> = new Set()
+
+    private registerMap = new Map<keyof EntityTypeMap, Set<EntityTypeMap[keyof EntityTypeMap]>>([
+        ["animal", this.animals],
+        ["enclosure", this.enclosures],
+        ["employee", this.employees],
+    ])
+
+    constructor(private warehouse: IWarehouse<T> & IRestockable<T>) { }
+
+    register<T extends keyof EntityTypeMap>(entityType: T, entity: EntityTypeMap[T]) {
+        this.registerMap.get(entityType)?.add(entity)
     }
 
     app() {
-        // const bobik = new Animal("bobik", HerbivoreSpecies.elephant)
-        // const bobik2 = new Animal("bobik2", HerbivoreSpecies.elephant)
-        //
-        // const hay = new Food(HerbivoreDiet.hay, 50)
-        // const vegetables = new Food(HerbivoreDiet.vegetables, 100)
-        //
-        // const enclosure = new Enclosure(2, [ bobik, bobik2 ])
-        //
-        // const assortment = [ hay, vegetables ]
-        // bobik.feedAssortment(assortment)
+        const bob = new Employee("Bob", "Animal Curator")
+        this.register("employee", bob)
 
-        // enclosure.showInhabitants()
+        const bobik = new Animal("bobik", HerbivoreSpecies.elephant)
+        const bobik2 = new Animal("bobik2", HerbivoreSpecies.elephant)
+
+        const hay = new Food(OmnivoreFood.hay, 50)
+        const vegetables = new Food(OmnivoreFood.vegetables, 100)
+
+        const enclosure = new Enclosure(2, [ bobik, bobik2 ])
+
+        const assortment = [ hay, vegetables ]
+        bob.feedAnimal(bobik, assortment)
+
+        enclosure.showInhabitants()
+    }
+
+    restock(factory: IFoodFactory, foodTypes: ICatalogue<getFoodTypes<T>>) {
+        this.warehouse.restock(factory, foodTypes)
     }
 }
